355bcd2715fd4770056919f1853cc7d63ccda617
Spiros Karampinis <44868218+Karspi@users.noreply.github.com>
2020-04-20 16:50:11 +0200

    Update IntuneDeployPrinters.ps1

--- IntuneDeployPrinters.ps1
+++ IntuneDeployPrinters.ps1
@@ -1,17 +1,10 @@
-
 <#
-
 .SYNOPSIS
 	This script is designed to automate the deployment of a printer to Windows 10 devices
 	by using a blob storage to store the printer driver files. 
 	The trigger for the creation was the deployment of a few printers over Intune. 
 
 .DESCRIPTION
-	Write by Spiros Karampinis, IT-Consultant
-	https://www.linkedin.com/in/spikar
-	Version 0.1 - 23.03.2020
-
-.NOTES
 	An Azure storage account, blob, will be needed to store the necessary files.
 	Please upload the .INF .CAT and .CAB file of the driver. 
 	Update the variables with your data. 
@@ -22,6 +15,11 @@
 	thereafter will proceed by adding the printer driver and at the end the printer at itself.
 
 	Please feel free to recommend any changes / improvements.
+	
+.NOTES
+	Written by Spiros Karampinis, IT-Consultant
+	https://www.linkedin.com/in/spikar
+	Version 0.1 - 23.03.2020
 #>
 
 # Azure blob storage folder
